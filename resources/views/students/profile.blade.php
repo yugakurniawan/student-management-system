f551b7429f0917046e5c97412a07e0cfc647c4a9
unknown <yugakurniawan@yahoo.com>
2020-09-15 16:39:36 +0700

    base profile construction

--- resources/views/students/profile.blade.php
+++ resources/views/students/profile.blade.php
@@ -41,22 +41,12 @@
                                     <li>Mobile <span>(124) 823409234</span></li>
                                     <li>Email <span>samuel@mydomain.com</span></li>
                                     <li>Website <span><a href="https://www.themeineed.com">www.themeineed.com</a></span>
+                                    <li>Website <span><a href="https://www.themeineed.com">www.themeineed.com</a></span>
+                                    <li>Website <span><a href="https://www.themeineed.com">www.themeineed.com</a></span>
                                     </li>
                                 </ul>
                             </div>
-                            <div class="profile-info">
-                                <h4 class="heading">Social</h4>
-                                <ul class="list-inline social-icons">
-                                    <li><a href="#" class="facebook-bg"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a href="#" class="twitter-bg"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#" class="google-plus-bg"><i class="fa fa-google-plus"></i></a></li>
-                                    <li><a href="#" class="github-bg"><i class="fa fa-github"></i></a></li>
-                                </ul>
-                            </div>
-                            <div class="profile-info">
-                                <h4 class="heading">About</h4>
-                                <p>Interactively fashion excellent information after distinctive outsourcing.</p>
-                            </div>
+
                             <div class="text-center"><a href="#" class="btn btn-primary">Edit Profile</a></div>
                         </div>
                         <!-- END PROFILE DETAIL -->
@@ -64,45 +54,7 @@
                     <!-- END LEFT COLUMN -->
                     <!-- RIGHT COLUMN -->
                     <div class="profile-right">
-                        <h4 class="heading">Samuel's Awards</h4>
-                        <!-- AWARDS -->
-                        <div class="awards">
-                            <div class="row">
-                                <div class="col-md-3 col-sm-6">
-                                    <div class="award-item">
-                                        <div class="hexagon">
-                                            <span class="lnr lnr-sun award-icon"></span>
-                                        </div>
-                                        <span>Most Bright Idea</span>
-                                    </div>
-                                </div>
-                                <div class="col-md-3 col-sm-6">
-                                    <div class="award-item">
-                                        <div class="hexagon">
-                                            <span class="lnr lnr-clock award-icon"></span>
-                                        </div>
-                                        <span>Most On-Time</span>
-                                    </div>
-                                </div>
-                                <div class="col-md-3 col-sm-6">
-                                    <div class="award-item">
-                                        <div class="hexagon">
-                                            <span class="lnr lnr-magic-wand award-icon"></span>
-                                        </div>
-                                        <span>Problem Solver</span>
-                                    </div>
-                                </div>
-                                <div class="col-md-3 col-sm-6">
-                                    <div class="award-item">
-                                        <div class="hexagon">
-                                            <span class="lnr lnr-heart award-icon"></span>
-                                        </div>
-                                        <span>Most Loved</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="text-center"><a href="#" class="btn btn-default">See all awards</a></div>
-                        </div>
+
                         <!-- END AWARDS -->
                         <!-- TABBED CONTENT -->
                         <div class="custom-tabs-line tabs-line-bottom left-aligned">
@@ -115,30 +67,52 @@
                         </div>
                         <div class="tab-content">
                             <div class="tab-pane fade in active" id="tab-bottom-left1">
-                                <ul class="list-unstyled activity-timeline">
-                                    <li>
-                                        <i class="fa fa-comment activity-icon"></i>
-                                        <p>Commented on post <a href="#">Prototyping</a> <span class="timestamp">2
-                                                minutes ago</span></p>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-cloud-upload activity-icon"></i>
-                                        <p>Uploaded new file <a href="#">Proposal.docx</a> to project <a href="#">New
-                                                Year Campaign</a> <span class="timestamp">7 hours ago</span></p>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-plus activity-icon"></i>
-                                        <p>Added <a href="#">Martin</a> and <a href="#">3 others colleagues</a> to
-                                            project repository <span class="timestamp">Yesterday</span></p>
-                                    </li>
-                                    <li>
-                                        <i class="fa fa-check activity-icon"></i>
-                                        <p>Finished 80% of all <a href="#">assigned tasks</a> <span class="timestamp">1
-                                                day ago</span></p>
-                                    </li>
-                                </ul>
-                                <div class="margin-top-30 text-center"><a href="#" class="btn btn-default">See all
-                                        activity</a></div>
+                                <div class="table-responsive">
+                                    <table class="table project-table">
+                                        <thead>
+                                            <tr>
+                                                <th>Semester</th>
+                                                <th>Nilai</th>
+                                                <a href="#" class="btn btn-primary">Tambah Nilai</a>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td><a href="#">Semester 1</a></td>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar" role="progressbar" aria-valuenow="60"
+                                                            aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                                            <span>3.4</span>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><a href="#">Semester 2</a></td>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar" role="progressbar" aria-valuenow="60"
+                                                            aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                                            <span>3.4</span>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td><a href="#">Semester 3</a></td>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar" role="progressbar" aria-valuenow="60"
+                                                            aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                                            <span>3.4</span>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                             <div class="tab-pane fade" id="tab-bottom-left2">
                                 <div class="table-responsive">
